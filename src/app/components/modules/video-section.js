8c19fa4c73f1235b30cf6715cbd7cdacaa7acfc5
Tim van Gemert <timvangemert@outlook.com>
2025-01-22 19:11:05 +0100

    remake video-section

--- src/app/components/modules/video-section.js
+++ src/app/components/modules/video-section.js
@@ -8,16 +8,16 @@ export default function VideoSection({
   title, 
   description,
   className = "", // Optional className prop for custom positioning
-  videoOnRight = true // New prop to control video position
+  isReversed = false // New prop to control section layout
 }) {
   return (
     <section className={`container relative w-full grid grid-cols-12 gap-6 items-center ${className}`}>
       {/* Video Section */}
-      <div className={`col-span-12 md:col-span-6 relative order-2 md:order-none before:absolute before:flex before:w-[90%] before:h-[90%] before:top-[-16px] md:before:top-[-32px] before:from-[#ADB1FF] before:via-[#ADB1FF] before:to-white before:opacity-20 before:z-[-1] before:rounded-3xl reveal-on-scroll ${
-        videoOnRight 
-          ? 'md:row-start-1 before:bg-gradient-to-br before:left-[-16px] md:before:left-[-32px]' 
-          : 'order-2 md:order-1 md:col-start-7 before:bg-gradient-to-bl before:right-[-16px] md:before:right-[-32px]'
-      }`}>
+      <div className={`order-first md:order-none col-span-12 md:col-span-6 relative ${
+        isReversed ? 'md:col-start-7 md:row-start-1' : ''
+      } before:absolute before:flex before:w-[90%] before:h-[90%] before:top-[-16px] md:before:top-[-32px] before:from-[#ADB1FF] before:via-[#ADB1FF] before:to-white before:opacity-20 before:z-[-1] before:rounded-3xl before:bg-gradient-to-br ${
+        isReversed ? 'before:right-[-16px] md:before:right-[-32px]' : 'before:left-[-16px] md:before:left-[-32px]'
+      } reveal-on-scroll`}>
         <Video 
           thumbnailUrl={thumbnailUrl}
           videoUrl={videoUrl}
@@ -26,8 +26,8 @@ export default function VideoSection({
       </div>
       
       {/* Text Section */}
-      <div className={`col-span-12 md:col-span-5 h-full flex flex-col justify-center items-start order-1 md:order-none mb-6 md:mb-0 ${
-        videoOnRight ? 'md:col-start-8' : 'order-1 md:order-2 '
+      <div className={`order-first md:order-none col-span-12 md:col-span-5 h-full flex flex-col justify-center items-start mb-6 md:mb-0 ${
+        isReversed ? 'md:row-start-1' : 'md:col-start-8'
       }`}>
         <h2 className="text-h2-sm md:text-h2-md lg:text-h2 reveal-on-scroll">
           {title}
--- src/app/page.js
+++ src/app/page.js
@@ -5,29 +5,29 @@ import VideoSection from './components/modules/video-section';
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-full min-h-screen overflow-hidden">
+    <main className="flex flex-col h-full overflow-hidden">
       <Hero />
-      
-      <section id="interviews" className="flex flex-col">
+    
 
-        <VideoSection 
+      <VideoSection 
           thumbnailUrl="/images/interview_annie-marie_rakhorst.jpeg"
           videoUrl="https://www.youtube.com/watch?v=BVdHrTfO2PE"
           title="Interview met Annie-Marie Rakhorst"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
           className="mt-[120px] md:mt-[100px] lg:mt-[200px]"
-          videoOnRight={false}
+          isReversed={false}
         />
         
-        {/* <VideoSection 
+        <VideoSection 
           thumbnailUrl="/images/interview_vroemvroemkoffie.webp"
           videoUrl="https://www.youtube.com/watch?v=BVdHrTfO2PE"
           title="Interview met een duurzame foodtruck eigenaar"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
           className="mt-[120px] md:mt-[100px] lg:mt-[180px]"
-        /> */}
+          isReversed={true}
+    
+        />
 
-      </section>
 
     </main>
   );
